e7f66db3a668f2263ac90694754e7a5214821ec6
Erik <esteinholtz@cloudera.com>
2025-01-17 22:41:05 +0100

    new wrapper

--- 1_app/wrapper.py
+++ 1_app/wrapper.py
@@ -1,1 +1,4 @@
-!streamlit run 1_app/app.py --server.port $CDSW_APP_PORT --server.address 127.0.0.1
+# !streamlit run 1_app/app.py --server.port $CDSW_APP_PORT --server.address 127.0.0.1
+import subprocess
+
+print(subprocess.run(["streamlit run ./1_app/app.py --server.port $CDSW_APP_PORT --server.address 127.0.0.1"], shell=True))
